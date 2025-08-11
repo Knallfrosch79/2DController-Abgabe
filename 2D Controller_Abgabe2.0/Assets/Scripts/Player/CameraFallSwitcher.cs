157837b07cef7c52af81950a5ae883a691efa2ac
Knallfrosch79 <leonjahn1999@gmail.com>
2025-08-11 17:26:56 +0200

    ajksbax

---
+++ 2D Controller_Abgabe2.0/Assets/Scripts/Player/CameraFallSwitcher.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Unity.Cinemachine;
+
+public class CameraFallOffset : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D playerRb;
+    [SerializeField] private CinemachineCamera cinemachineCam;
+    [SerializeField] private CinemachineFollow followComponent;
+    [SerializeField] private float fallThreshold = -2f;
+    [SerializeField] private Vector3 fallOffset = new Vector3(0.3f, -1f, -10f);
+    [SerializeField] private Vector3 normalOffset = new Vector3(0.3f, 1.6f, -10f);
+    [SerializeField] private float lerpSpeed = 5f;
+
+    private void Update()
+    {
+        Vector3 targetOffset = playerRb.linearVelocity.y < fallThreshold ? fallOffset : normalOffset;
+        followComponent.FollowOffset = Vector3.Lerp(followComponent.FollowOffset, targetOffset, Time.deltaTime * lerpSpeed);
+    }
+}
